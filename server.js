b7b9507e1ccf5531eba79c0063e7517d3f8ac6f3
YoonJunHyeon <qmdldos@gmail.com>
2023-11-13 12:31:13 +0900

    .env 파일 생성 및 변수 저장 관리완료

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+// .env 파일 생성 및 다뤄보기
+require('dotenv').config(); // 환경 변수 설정
+
 const express = require('express');
 const app = express();
 
@@ -7,8 +10,7 @@ app.get('/', (req, res)=>{
   res.send('hello world');
 });
 
-const Port = process.env.Port || 3000;
+const Port = process.env.Port;
 app.listen(Port, ()=>{
   console.log(`http://localhost:${Port}`);
 })
-s
