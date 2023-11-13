b0dbc06d57e820451524e3a57b40bce9adf12654
YoonJunHyeon <qmdldos@gmail.com>
2023-11-13 09:43:41 +0900

    uri 테스트

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
-console.log('hi');
+require('dotenv').config(); 
+// const { MongoClient } = require("mongodb");
 
-const client = new 
+console.log('hi');
+const uri = process.env.MONGODB_URI;
+console.log(uri);
+// const client = new MongoClient(uri);
