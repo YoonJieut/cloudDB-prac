a7bbd593c7bc3033048bfe61dba1eff36e9f774a
YoonJunHyeon <qmdldos@gmail.com>
2023-11-13 12:07:47 +0900

    다시 시작한다.

--- app.js
+++ app.js
@@ -1,7 +1,1 @@
-require('dotenv').config(); 
-// const { MongoClient } = require("mongodb");
-
 console.log('hi');
-const uri = process.env.MONGODB_URI;
-console.log(uri);
-// const client = new MongoClient(uri);
