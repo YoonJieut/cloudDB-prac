613da23aec1204b4ee5fc455f418e3ac24843ed2
YoonJunHyeon <qmdldos@gmail.com>
2023-11-13 12:44:38 +0900

    연결 성공 표시

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ require('dotenv').config(); // 환경 변수 설정
 
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.uri) // 연결 성공 
+  .then(()=>{ console.log("연결 성공",)}) 
+  .catch((err)=>{ console.error("연결 실패", err)});
 
 app.use(express.json());// JSON 요청 본문을 처리하기 위한 미들웨어
 
