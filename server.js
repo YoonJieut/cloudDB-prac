5cd3df6e908895267b5a86aa60ff203be4ecdad8
YoonJunHyeon <qmdldos@gmail.com>
2023-11-13 12:25:41 +0900

    메인 라우팅 설정

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());// JSON 요청 본문을 처리하기 위한 미들웨어
+
+app.get('/', (req, res)=>{
+  res.send('hello world');
+});
+
+
